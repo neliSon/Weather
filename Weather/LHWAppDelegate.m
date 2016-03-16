06674bc3e2c1b37b340d4e3af19691a94b73fe37
neliSun <nelson.chow91.nc@gmail.com>
2016-03-16 11:12:18 -0700

    Initial Commit

---
+++ Weather/LHWAppDelegate.m
@@ -0,0 +1,24 @@
+//
+//  LHWAppDelegate.m
+//  Weather
+//
+//  Created by Steven Masuch on 2014-07-30.
+//  Copyright (c) 2014 Lighthouse Labs. All rights reserved.
+//
+
+#import "LHWAppDelegate.h"
+
+@implementation LHWAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    
+    // Your code goes here
+    // Don't forget to assign the window a rootViewController
+    
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end
